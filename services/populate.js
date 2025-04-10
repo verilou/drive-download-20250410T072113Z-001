b707fd4470a46db023c87a83474f583e83fef06f
Louis Castel <louis.castel31@gmail.com>
2025-04-10 12:16:52 +0200

    feat: edit filter on game population

--- services/populate.js
+++ services/populate.js
@@ -28,13 +28,21 @@ const getStreamAndPopulate = async (path, platform) => {
   https.get(path, async (res) => {
     res.pipe(StreamArray.withParser()).on('data', async (data) => {
       const games = data.value
-        .filter((game) => game.app_id !== undefined)
+        .filter(
+          (game) =>
+            game.publisher_id !== undefined ||
+            game.name !== undefined ||
+            game.app_id !== undefined ||
+            game.bundle_id !== undefined ||
+            game.version !== undefined ||
+            game.release_date !== undefined,
+        ) // this is an aggressive filter, we should use a more sophisticated one
         .map((game) => {
           return {
             publisherId: game.publisher_id,
             name: game.name,
             platform,
-            storeId: game.app_id ?? game.appId ?? game.bundle_id ?? game.id,
+            storeId: game.app_id,
             bundleId: game.bundle_id,
             appVersion: game.version,
             isPublished: new Date(game.release_date) <= new Date(),
