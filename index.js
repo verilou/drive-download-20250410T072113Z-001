6180d1150cc19f6d38cca65612a8dc4449fab2a2
Louis Castel <louis.castel31@gmail.com>
2025-04-10 09:40:57 +0200

    chore:add prettier and format files

--- index.js
+++ index.js
@@ -7,17 +7,35 @@ const app = express();
 app.use(bodyParser.json());
 app.use(express.static(`${__dirname}/static`));
 
-app.get('/api/games', (req, res) => db.Game.findAll()
-  .then(games => res.send(games))
-  .catch((err) => {
-    console.log('There was an error querying games', JSON.stringify(err));
-    return res.send(err);
-  }));
+app.get('/api/games', (req, res) =>
+  db.Game.findAll()
+    .then((games) => res.send(games))
+    .catch((err) => {
+      console.log('There was an error querying games', JSON.stringify(err));
+      return res.send(err);
+    }),
+);
 
 app.post('/api/games', (req, res) => {
-  const { publisherId, name, platform, storeId, bundleId, appVersion, isPublished } = req.body;
-  return db.Game.create({ publisherId, name, platform, storeId, bundleId, appVersion, isPublished })
-    .then(game => res.send(game))
+  const {
+    publisherId,
+    name,
+    platform,
+    storeId,
+    bundleId,
+    appVersion,
+    isPublished,
+  } = req.body;
+  return db.Game.create({
+    publisherId,
+    name,
+    platform,
+    storeId,
+    bundleId,
+    appVersion,
+    isPublished,
+  })
+    .then((game) => res.send(game))
     .catch((err) => {
       console.log('***There was an error creating a game', JSON.stringify(err));
       return res.status(400).send(err);
@@ -28,7 +46,7 @@ app.delete('/api/games/:id', (req, res) => {
   // eslint-disable-next-line radix
   const id = parseInt(req.params.id);
   return db.Game.findByPk(id)
-    .then(game => game.destroy({ force: true }))
+    .then((game) => game.destroy({ force: true }))
     .then(() => res.send({ id }))
     .catch((err) => {
       console.log('***Error deleting game', JSON.stringify(err));
@@ -39,18 +57,33 @@ app.delete('/api/games/:id', (req, res) => {
 app.put('/api/games/:id', (req, res) => {
   // eslint-disable-next-line radix
   const id = parseInt(req.params.id);
-  return db.Game.findByPk(id)
-    .then((game) => {
-      const { publisherId, name, platform, storeId, bundleId, appVersion, isPublished } = req.body;
-      return game.update({ publisherId, name, platform, storeId, bundleId, appVersion, isPublished })
-        .then(() => res.send(game))
-        .catch((err) => {
-          console.log('***Error updating game', JSON.stringify(err));
-          res.status(400).send(err);
-        });
-    });
+  return db.Game.findByPk(id).then((game) => {
+    const {
+      publisherId,
+      name,
+      platform,
+      storeId,
+      bundleId,
+      appVersion,
+      isPublished,
+    } = req.body;
+    return game
+      .update({
+        publisherId,
+        name,
+        platform,
+        storeId,
+        bundleId,
+        appVersion,
+        isPublished,
+      })
+      .then(() => res.send(game))
+      .catch((err) => {
+        console.log('***Error updating game', JSON.stringify(err));
+        res.status(400).send(err);
+      });
+  });
 });
-
 
 app.listen(3000, () => {
   console.log('Server is up on port 3000');
